5275d8685464be632d8c25c7832fdbd38b1fda72
Fedodv <psgameeasp@gmail.com>
2025-12-10 00:05:10 +0300

    fix deploy 4

--- weather-backend/src/weather/weather.service.ts
+++ weather-backend/src/weather/weather.service.ts
@@ -7,27 +7,39 @@ import { ConfigService } from '../config/config.service';
 
 @Injectable()
 export class WeatherService {
-    constructor (
-        @InjectModel(WeatherCache.name) private cacheModel: Model<WeatherCache>,
-        private cfg: ConfigService
-    ) {}
+  constructor(
+    @InjectModel(WeatherCache.name) private cacheModel: Model<WeatherCache>,
+    private cfg: ConfigService,
+  ) {}
 
-    async getWeather(city: string) {
-        const cached = await this.cacheModel.findOne({ city });
+  async getWeather(city: string) {
+    if (!city) throw new Error('City is required');
 
-        if (cached && cached.updatedAt > new Date(Date.now()- 60000)) {
-            return cached.data;
-        }
-
-        const url = `https://api.weatherapi.com/v1/current.json?key=${this.cfg.weatherApiKey}&q=${city}`;
-        const response = await axios.get(url);
-
-        await this.cacheModel.findOneAndUpdate(
-            {city},
-            {data: response.data, updatedAt: new Date()},
-            {upsert: true}
-        );
-
-        return response.data
+    const cached = await this.cacheModel.findOne({ city });
+    if (cached && cached.updatedAt > new Date(Date.now() - 60000)) {
+      return cached.data;
     }
+
+    try {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(
+        city,
+      )}&appid=${this.cfg.weatherApiKey}&units=metric`;
+
+      console.log('Request URL:', url);
+
+      const { data } = await axios.get(url);
+
+   
+      await this.cacheModel.findOneAndUpdate(
+        { city },
+        { data, updatedAt: new Date() },
+        { upsert: true },
+      );
+
+      return data;
+    } catch (err: any) {
+      console.error('Weather API Error:', err.response?.data || err.message);
+      throw new Error('Failed to fetch weather');
+    }
+  }
 }
